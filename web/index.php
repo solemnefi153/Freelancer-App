8c1ec502ddeec25c2fa3338c5633a89f94eb4c57
Nefi Aguilar <solemnefi153@gmail.com>
2020-11-01 20:17:46 -0700

    Logic to redirect to sign in page

--- web/index.php
+++ web/index.php
@@ -3,9 +3,8 @@
 <?php
    if($_SESSION['active_login'] != true)
    {
-      echo "this is happening";
       #If there is not an active session then redirect to the login page
-      #header ("Location: http://localhost:8000/web/views/sign_in.php");
+      header ("Location: https://freelancer-path.herokuapp.com/views/sign_in.php");
    }
    else{
       #if there is an active session check if the session is for a client or an admin 
