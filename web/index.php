f93a88731517321f323dac432f812c1e40c49ddd
Nefi Aguilar <solemnefi153@gmail.com>
2020-11-01 20:15:35 -0700

    Restarted Project

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
    {
       echo "this is happening";
       #If there is not an active session then redirect to the login page
-      header ("Location: http://localhost:8000/web/views/sign_in.php");
+      #header ("Location: http://localhost:8000/web/views/sign_in.php");
    }
    else{
       #if there is an active session check if the session is for a client or an admin 
